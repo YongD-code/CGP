36165394bfbf47cb56f00722b8bd6bdc8728840e
Woong0810 <chan081010@naver.com>
2025-11-25 05:01:19 +0900

    boxes getter함수 추가

--- Computer_Graphics_Termp/Map.h
+++ Computer_Graphics_Termp/Map.h
@@ -33,6 +33,14 @@ public:
     // 2D 배열에따라 3D 맵을 렌더링하는 함수
     void InitFromArray(int w, int h, const int* data);
 
+    // C++에서는 보통 클래스의 멤버 변수를 private: 영역에 둠
+    // private: 영역에 있는 변수나 함수들은 클래스 외부에서 접근이 불가능함 (friend 키워드 쓰지 않는이상)
+    // 그래서 보통 private: 영역에 있는 멤버 변수 값을 받아오기 위해서 접근 가능한 public: 영역에 getter 함수를 사용함
+    const std::vector<Box>& GetBoxes() const
+    {
+        return boxes;
+    }
+
 private:
     std::vector<Box> boxes;
 };
