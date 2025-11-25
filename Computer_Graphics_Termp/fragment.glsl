d7445f2e291f236ea19a99c8bfbfed2a4e6ab69b
KIM YONG HYUN <dydrkwl@gmail.com>
2025-11-26 01:07:47 +0900

    조명 처리를 위한 fragment, vertex 수정

--- Computer_Graphics_Termp/fragment.glsl
+++ Computer_Graphics_Termp/fragment.glsl
@@ -1,11 +1,34 @@
 #version 330 core
 
-in vec3 vNormal;
-out vec4 FragColor;
+in vec3 FragPos;
+in vec3 Normal;
 
-uniform vec3 uColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform vec3 objectColor;
+
+out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(uColor, 1.0);
+    // Ambient
+    float ambientStrength = 0.25;
+    vec3 ambient = ambientStrength * objectColor;
+
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * objectColor;
+
+    // Specular
+    float specStrength = 0.35;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16);
+    vec3 specular = specStrength * spec * vec3(1.0);
+
+    // final
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
 }
--- Computer_Graphics_Termp/vertex.glsl
+++ Computer_Graphics_Termp/vertex.glsl
@@ -3,7 +3,8 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 
-out vec3 vNormal;
+out vec3 FragPos;
+out vec3 Normal;
 
 uniform mat4 uModel;
 uniform mat4 uView;
@@ -11,9 +12,10 @@ uniform mat4 uProj;
 
 void main()
 {
-    gl_Position = uProj * uView * uModel * vec4(aPos, 1.0);
+    vec4 worldPos = uModel * vec4(aPos, 1.0);
+    FragPos = worldPos.xyz;
 
-    // 나중에 조명을 위해
-    mat3 normalMat = mat3(transpose(inverse(uModel)));
-    vNormal = normalize(normalMat * aNormal);
+    Normal = mat3(transpose(inverse(uModel))) * aNormal;
+
+    gl_Position = uProj * uView * worldPos;
 }
