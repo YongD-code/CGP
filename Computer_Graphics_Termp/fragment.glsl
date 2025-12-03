879d451c37e11512e8ea482f101e3aaf73722e31
KIM YONG HYUN <dydrkwl@gmail.com>
2025-12-03 19:36:48 +0900

    방 어둡게 fragment 처리 총이랑 레이저는 보이도록 예외처리

--- Computer_Graphics_Termp/fragment.glsl
+++ Computer_Graphics_Termp/fragment.glsl
@@ -6,6 +6,7 @@ in vec3 Normal;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 uniform vec3 objectColor;
+uniform bool uDarkMode;
 
 out vec4 FragColor;
 
@@ -31,4 +32,26 @@ void main()
     // final
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
+
+    if (uDarkMode) {
+    // 스캔된 점은 원래색 유지
+    if (objectColor == vec3(0.0, 1.0, 0.4)) {
+        FragColor = vec4(objectColor, 1.0);
+        return;
+    }
+    //총은 보이도록 
+    if (objectColor == vec3(0.25, 0.25, 0.25)) {
+        FragColor = vec4(objectColor, 1.0);
+        return;
+    }
+    // 레이저 광선도 보이도록
+    if (objectColor == vec3(1.0, 0.0, 0.0)) {
+        FragColor = vec4(objectColor, 1.0);
+        return;
+    }
+    // 그 외는 어둡게
+    FragColor = vec4(objectColor * 0.03, 1.0);
+    return;
+}
+
 }
